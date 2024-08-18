84d56fc5465b1d2b313c8ff1ced3a246316b4af0
ShamimRahimi <119719139+ShamimRahimi@users.noreply.github.com>
2024-08-18 14:35:54 +0330

    added login and signup view

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
+from uuid import uuid4
 
 # Create your models here.
 class App(models.Model):
@@ -10,8 +14,26 @@ class App(models.Model):
         ('error', 'Error'),
         ('offline', 'Offline'),
     ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     state = models.CharField(max_length=10, choices=STATE_CHOICES, default='offline')
     size = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
+    
+
+class Token(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    key = models.CharField(max_length=255, unique=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    expiration_time = models.DateTimeField()
+
+    def is_expired(self):
+        return timezone.now() > self.expiration_time
+    
+    @classmethod
+    def generate_token(cls, user):
+        key = uuid4()
+        expiration_time = timezone.now() + timedelta(hours=1)
+        return cls.objects.create(user=user, key=key, expiration_time=expiration_time)
